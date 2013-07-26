ce4e26bc88278b307a7f3a2e2884605f203354c6
alnutile <alfrednutile@gmail.com>
2013-07-26 16:18:50 -0400

    LinkedIn Installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkedin::Application.config.secret_token = '8c08116e101d8c201e828e368837c336135e817175acd1c9a7c571b1d3d60bd579e542db49a39dae90b84b7484631fbb55787457b1fceda1cdb1b7a07091ec78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Linkedin::Application.config.session_store :cookie_store, key: '_linkedin_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Linkedin::Application.config.session_store :active_record_store
