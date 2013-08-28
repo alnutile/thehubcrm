3fc958f8014558035f25e4e0960df72a7e51f785
alnutile <alfrednutile@gmail.com>
2013-08-27 22:10:59 -0400

    added image to the top part of the note

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
   gem 'angularjs-rails'
   gem 'angular-ui-rails'
+  gem 'jquery_datepicker'
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
 
--- app/assets/javascripts/dash_non_angular.js
+++ app/assets/javascripts/dash_non_angular.js
@@ -1,6 +1,11 @@
  $(document).ready(function() {
-  
-  	$('#action_date').datetimepicker();
+
+    //@todo get this to work
+    //  $('#action_date').datetimepicker();
+    //  $('#action_date').on('mouseout', function(){
+    //      console.log($(this).val());
+    //      $('#action_date').val($(this).val())
+    //  });
   	//$('#message').wysihtml5();
   	$('.people-search').focus();
   });
