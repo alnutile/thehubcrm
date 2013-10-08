90e83e4dcdfb2abae2a7a2e35e2d8b6f8c540cac
alnutile <alfrednutile@gmail.com>
2013-10-08 17:28:38 -0400

    Add config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Linkedin::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
